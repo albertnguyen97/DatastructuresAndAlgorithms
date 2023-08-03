d36b00d96754e5675d56cc6416e6520a6798612a
albertnguyen97 <chaunm.hmc@gmail.com>
2023-08-03 23:30:48 +0700

    add circulardoublelinkedlist and circularsinglelinkedlist

---
+++ circularSingleLinkedList.py
@@ -0,0 +1,114 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class CircularSinglyLinkedList:
+    def __int__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            if node.next == self.head:
+                break
+            node = node.next
+
+    def createCSLL(self, nodeValue):
+        node = Node(nodeValue)
+        node.next = node
+        self.head = node
+        self.tail = node
+        return "The CSLL has been created"
+
+    def insertCSLL(self, value, location):
+        if self.head is None:
+            return "Is None"
+        else:
+            newNode = Node(value)
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+                self.tail.next = newNode
+            elif location == 1:
+                newNode.next = self.tail.next
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = newNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+            return "inserted node"
+
+    def printCSLL(self):
+        if self.head is None:
+            print("dont have any element in CSLL")
+        else:
+            tempNode = self.head
+            while tempNode:
+                print(tempNode.value)
+                tempNode = tempNode.next
+                if tempNode == self.tail.next:
+                    break
+
+    def searchCSLL(self, nodeValue):
+        if self.head is None:
+            return "dont have any node in CSLL"
+        else:
+            tempNode = self.head
+            while tempNode:
+                if tempNode.value == nodeValue:
+                    return tempNode.value
+                tempNode = tempNode.next
+                if tempNode == self.tail.next:
+                    return "the node doesn't exist in this CSLL"
+
+    def deleteNode(self, location):
+        if self.head is None:
+            print("the is not any node here")
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+                    self.tail.next = self.head
+            elif location == 1:
+                if self.head == self.tail:
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    node = self.head
+                    while node is not None:
+                        if node.next == self.tail:
+                            break
+                        node = node.next
+                    node.next = self.head
+                    self.tail = node
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = nextNode.next
+
+    def deleteAllCSLL(self):
+        self.head = None
+        self.tail.next = None
+        self.tail = None
+
+
+circularSLL = CircularSinglyLinkedList()
+circularSLL.createCSLL(1)
