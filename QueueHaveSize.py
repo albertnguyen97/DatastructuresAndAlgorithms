3e755b1e394e039431de4aa77bc2175570651554
albertnguyen97 <chaunm.hmc@gmail.com>
2023-08-04 19:29:27 +0700

    add Queue list

---
+++ QueueHaveSize.py
@@ -0,0 +1,64 @@
+class Queue:
+    def __init__(self, size):
+        self.items = size * [None]
+        self.size = size
+        self.start = -1
+        self.top = -1
+
+    def __str__(self):
+        values = [str(x) for x in self.items]
+        return ' '.join(values)
+
+    def isFull(self):
+        if self.top + 1 == self.start:
+            return True
+        elif self.start == 0 and self.top + 1 == self.size:
+            return True
+        else:
+            return False
+
+    def isEmpty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
+
+    def enqueue(self, value):
+        if self.isFull():
+            return "The queue is full"
+        else:
+            if self.top + 1 == self.size:
+                self.top = 0
+            else:
+                self.top += 1
+                if self.start == -1:
+                    self.start = 0
+            self.items[self.top] = value
+            return "inserted"
+
+    def dequeue(self):
+        if self.isEmpty():
+            return "queue is empty"
+        else:
+            firstElement = self.items[self.start]
+            start = self.start
+            if self.start == self.top:
+                self.start = -1
+                self.top = -1
+            elif self.start + 1 == self.size:
+                self.start = 0
+            else:
+                self.start += 1
+            self.items[start] = None
+            return firstElement
+
+    def peek(self):
+        if self.isEmpty():
+            return "queue is empty"
+        else:
+            return self.items[self.start]
+
+    def delete(self):
+        self.items = self.size * [None]
+        self.top = -1
+        self.start = -1
