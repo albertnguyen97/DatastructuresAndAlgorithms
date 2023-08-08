d8ad3a64ae746e1994107376d70f49135f8f281d
albertnguyen97 <chaunm.hmc@gmail.com>
2023-08-08 23:56:11 +0700

    add treelist

--- Tree/TreeLinkedList.py
+++ Tree/TreeLinkedList.py
@@ -47,8 +47,81 @@ def levelOrderTraversal(rootNode):
                 customQueue.enqueue(root.value.rightChild)
 
 
+def searchBT(rootNode, nodeValue):
+    if not rootNode:
+        return "the BT not exist"
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not customQueue.isEmpty():
+            root = customQueue.dequeue()
+            if root.value.data == nodeValue:
+                return "Success"
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+        return "not found"
+
+
+def insertTB(rootNode, newNode):
+    if not newNode:
+        return "the BT not exist"
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not customQueue.isEmpty():
+            root = customQueue.dequeue()
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            else:
+                root.value.leftChild = newNode
+                return "Inserted"
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+            else:
+                root.value.rightChild = newNode
+                return "Inserted"
+
+def getDeepestNode(rootNode):
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not customQueue.isEmpty():
+            root = customQueue.dequeue()
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+        deepestNode = root.value
+        return deepestNode
+
+def deleteDeepestNode(rootNode, dNode):
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not customQueue.isEmpty():
+            root = customQueue.dequeue()
+            if root.value is dNode:
+                root.value = None
+                return
+            if root.value.rightChild:
+                if root.value.rightChild is dNode:
+                    root.value.rightChild = None
+                    return
+                else:
+                    customQueue.enqueue(root.value.rightChild)
+
+
 myTree = TreeNode("1")
 myTree.leftChild = TreeNode("2")
 myTree.rightChild = TreeNode("3")
 levelOrderTraversal(myTree)
 inOrderTraversal(myTree)
+deepestNode = getDeepestNode(myTree)
+print(deepestNode.data)
+
---
+++ Tree/TreeList.py
@@ -0,0 +1,30 @@
+class BinaryTree:
+    def __init__(self, size):
+        self.customList = size * [None]
+        self.lastUsedIndex = 0
+        self.maxSize = size
+
+    def insertNode(self, value):
+        if self.lastUsedIndex + 1 == self.maxSize:
+            return "fulled"
+        self.customList[self.lastUsedIndex + 1] = value
+        self. lastUsedIndex += 1
+        return "inserted"
+
+    def searchNode(self, nodeValue):
+        for i in range(len(self.customList)):
+            if self.customList[i] == nodeValue:
+                return "successful"
+        return "not found"
+
+    def preOrderTraversal(self, index):
+        if index > self.lastUsedIndex:
+            return
+        print(self.customList[index])
+        self.preOrderTraversal(index*2)
+        self.preOrderTraversal(index*2 + 1)
+
+
+newTree = BinaryTree(8)
+newTree.insertNode("me")
+newTree.insertNode("you")
