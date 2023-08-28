73ec71dc1115c778396e8c80ed2c1e792ca8d716
albertnguyen97 <chaunm.hmc@gmail.com>
2023-08-28 23:48:55 +0700

    add delete

--- Tree/BST.py
+++ Tree/BST.py
@@ -1,5 +1,6 @@
 import QueueLinkedList as queue
 
+
 class BTSNode:
     def __init__(self, data):
         self.data = data
@@ -61,7 +62,7 @@ def levelOrderTraversal(rootNode):
     else:
         customQueue = queue.Queue()
         customQueue.enqueue(rootNode)
-        while not(customQueue.isEmpty()):
+        while not (customQueue.isEmpty()):
             root = customQueue.dequeue()
             print(root.value.data)
             if root.value.leftChild is not None:
@@ -83,3 +84,40 @@ def searchNode(rootNode, nodeValue):
             print("the value is found")
         else:
             searchNode(rootNode.rightChild, nodeValue)
+
+
+def minValueNode(bstNode):
+    current = bstNode
+    while current.leftChild is not None:
+        current = current.leftChild
+    return current
+
+
+def deleteNode(rootNode, nodeValue):
+    if rootNode is None:
+        return rootNode
+    if nodeValue < rootNode.data:
+        rootNode.leftChild = deleteNode(rootNode.leftChild, nodeValue)
+    elif nodeValue > rootNode.data:
+        rootNode.rightChild = deleteNode(rootNode.rightChild, nodeValue)
+    else:
+        if rootNode.leftChild is None:
+            temp = rootNode.rightChild
+            rootNode = None
+            return temp
+        if rootNode.rightChild is None:
+            temp = rootNode.leftChild
+            rootNode = None
+            return temp
+
+        temp = minValueNode(rootNode.rightChild)
+        rootNode.data = temp.data
+        rootNode.rightChild = deleteNode(rootNode.rightChild, temp.data)
+    return rootNode
+
+
+def deleteBST(rootNode):
+    rootNode.data = None
+    rootNode.rightChild = None
+    rootNode.leftChild = None
+    return "deleted"
